e379bb252e20e0865aeee5bbfc63f21109fc3853
AntoineSPR <antoine.simper@gmail.com>
2025-08-12 19:50:50 +0200

    Get user data from local storage on homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { QuestContainerComponent } from './components/quest-container/quest-container.component';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,17 @@ import { QuestContainerComponent } from './components/quest-container/quest-cont
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private readonly _userService = inject(UserService);
+  ngOnInit() {
+    localStorage.getItem('user');
+    localStorage.getItem('token');
+
+    if (!localStorage.getItem('user') || !localStorage.getItem('token')) {
+      return;
+    }
+
+    this._userService.user.set(JSON.parse(localStorage.getItem('user') || 'null'));
+    this._userService.token.set(localStorage.getItem('token'));
+  }
+}
--- src/app/guards/is-logged-in.guard.ts
+++ src/app/guards/is-logged-in.guard.ts
@@ -1,15 +1,9 @@
-import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
-import { UserService } from '../services/user.service';
 
 export const isLoggedInGuard: CanActivateFn = (route, state) => {
-  const _userService = inject(UserService);
-
-  return _userService.user() !== null && _userService.token() !== null;
+  return localStorage.getItem('user') !== null && localStorage.getItem('token') !== null;
 };
 
 export const isLoggedOutGuard: CanActivateFn = (route, state) => {
-  const _userService = inject(UserService);
-
-  return _userService.user() === null || _userService.token() === null;
+  return localStorage.getItem('user') === null || localStorage.getItem('token') === null;
 };
